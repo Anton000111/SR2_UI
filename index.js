955ae2d9160097caa8e0283daa9d28e98409d4c2
Anton Mikheyeu <pro100633@gmail.com>
2023-05-11 17:11:35 +0300

    feat: custom ports

--- index.js
+++ index.js
@@ -4,14 +4,30 @@ import express from 'express';
 import open from 'open';
 import fs from 'node:fs';
 
-const PORT = 5000;
-
 const rootExecFolderPathArr = process.env._.split('/');
 
 rootExecFolderPathArr.pop();
 rootExecFolderPathArr.pop();
 
 const publicFolder = rootExecFolderPathArr.join('/') + '/lib/node_modules/sr2_ui/build';
+
+const pathToConfig = publicFolder + '/config.json';
+
+const config = JSON.parse(fs.readFileSync(pathToConfig));
+
+
+let PORT = 5000;
+
+const portFromEnv = process.argv.find(value => value.startsWith('PORT='));
+
+if (portFromEnv) PORT = Number(portFromEnv.split('=')[1]);
+
+const serverPortFromEnv = process.argv.find(value => value.startsWith('SERVER_PORT='));
+
+if (serverPortFromEnv) {
+  const SERVER_PORT = serverPortFromEnv.split('=')[1];
+  fs.writeFileSync(pathToConfig, JSON.stringify({ ...config, SERVER_PORT }));
+}
 
 const app = express();
 
